cb36a99233ee634adbfd2f3fc16a02f92ce7645b
MaharshiReddyP <maharshireddy.palavala@verifone.com>
2025-01-29 13:10:35 +0530

    Initial commit

---
+++ app/src/main/java/com/example/dependency/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.dependency;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import com.example.mylibrary.ToastHelper;
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        ToastHelper toastHelper=new ToastHelper();
+        toastHelper.display();
+    }
+}
